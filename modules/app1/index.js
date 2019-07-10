3782349005fa889a7b5d1a81883bc4a6ade5d1bf
lyxxxx <lyxxxx@yeah.net>
2019-07-11 00:06:19 +0800

    feat(project): 完成所有模块初版

---
+++ modules/app1/index.js
@@ -0,0 +1,8 @@
+class Dashboard {
+    constructor(ele) {
+        this.ele = ele
+    }
+
+}
+
+window.AstrolabePlan.register('Dashboard', Dashboard)
---
+++ modules/app2/index.js
@@ -0,0 +1,8 @@
+class App1 {
+    constructor(ele) {
+        this.ele = ele
+    }
+
+}
+
+window.AstrolabePlan.register('App-1', App1)
